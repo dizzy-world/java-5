bb68363d8edebcc0ba5ce449f8c3dd17f04309fa
yez <yez_iam1@mail.ru>
2026-01-17 16:09:21 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,20 @@
+let users = {};
+
+let count = Number(prompt("Введите количество пользователей (от 1 до 10):"));
+
+for (let i = 1; i <= count; i++) {
+  let name = prompt(`Введите имя`);
+  let age = Number(prompt(`Введите возраст`));
+
+  users[`user${i}`] = {
+    name: name,
+    age: age
+  };
+
+  console.log(`Пользователь - ${i}`);
+  console.log(`Имя - ${name}`);
+  console.log(`Возраст - ${age}`);
+}
+
+console.log("Весь объект пользователей:");
+console.log(users);
